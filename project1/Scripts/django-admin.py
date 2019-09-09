81ee8916ec16b2816f3e674fdc82f2de2b1561ab
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-09-09 11:52:05 -0700

    setup django python project

---
+++ project1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kessl\onedrive\desktop\the-tech-academy-python-coding-projects\project1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
